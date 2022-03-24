34f011ac96cfa22443ef7a3b0b805b8311c16bec
Gracmit <aleksii97@gmail.com>
2022-03-24 11:04:43 +0200

    Information of combat units

---
+++ Assets/Scripts/CombatUnit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class CombatUnit
+{
+    [SerializeField] private int _health;
+    [SerializeField] private int _maxHealth;
+    [SerializeField] private int _attack;
+    [SerializeField] private int _defence;
+    [SerializeField] private GameObject _combatAvatar;
+}
+
+public class Enemy : CombatUnit
+{
+    
+}
+
+public class PartyMember : CombatUnit
+{
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private List<Enemy> _enemies;
+    [SerializeField] private List<PartyMember> _party;
+}
